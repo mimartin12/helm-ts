7758365a100283b03eed998a9cd738e0846d632f
Micaiah Martin <mmartin@bitwarden.com>
2022-03-28 14:58:48 -0600

    Added uninstall command.

---
+++ src/commands/uninstall-cmd.ts
@@ -0,0 +1,29 @@
+import { UninstallFlags } from "../types";
+import { getAliasedGlobalFlags } from "./helm-cmd";
+import { runCommand, buildFlagsString } from "../utils/helpers";
+
+const buildHelmUninstallCmd = (
+    name: string,
+    flags: UninstallFlags = {},
+) => {
+    const allFlags = {
+        ...getAliasedGlobalFlags(flags),
+    };
+    const flagsString = buildFlagsString(allFlags);
+    return `helm uninstall ${name} ${flagsString}`
+};
+
+/**
+ * This command uninstalls a helm chart.
+ */
+
+const uninstall = async (
+    name: string,
+    flags: UninstallFlags = {}
+): Promise<{ stdout: string }> => {
+    const command = buildHelmUninstallCmd(name, flags);
+    const stdout = await runCommand(command);
+    return { stdout };
+}
+
+export default uninstall;
